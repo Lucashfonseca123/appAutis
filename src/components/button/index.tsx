8327bedf793baa3b3bc539aa3ea1168064acddfe
lucas.fonseca <lucas.fonseca@trizy.com.br>
2020-05-28 23:21:41 -0300

    Atualizado react native para ultima versão, sem splashScreen

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,46 +1,46 @@
-import React, { forwardRef } from 'react';
-import { TouchableOpacity } from 'react-native';
-import { Markdown, Image } from "../";
-import { Container, PaddingButton } from './styles'
+import React, {forwardRef} from 'react';
+import {TouchableOpacity} from 'react-native';
+import {Markdown, Image} from '../';
+import {Container, PaddingButton} from './styles';
 
 interface IButton {
-    text: string;
-    onPress: Function;
-    disabled?: boolean;
-    widthSize?: number | string;
-    heightSize?: number | string;
-    fontSize?: number;
-    backgroundColor?: string;
-    paddingLeft?: number;
-    paddingRight?: number;
-    color?: 'disabled' | 'enable';
+  text: string;
+  onPress: Function;
+  disabled?: boolean;
+  widthSize?: number | string;
+  heightSize?: number | string;
+  fontSize?: number;
+  backgroundColor?: string;
+  paddingLeft?: number;
+  paddingRight?: number;
+  color?: 'disabled' | 'enable';
 }
 
 const Button = (props: IButton) => {
-    return (
-        <Container color={props.color} widthSize={props.widthSize}>
-            <TouchableOpacity
-                disabled={props.disabled}
-                onPress={props.onPress ? props.onPress : null}
-                style={{
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    width: props.widthSize,
-                    paddingTop: props.heightSize,
-                    paddingBottom: props.heightSize,
-                    flexDirection: 'row'
-                }}>
-                {props.text === 'Menu' ?
-                    <>
-                        <PaddingButton>
-                            <Image type="Menu" width={20} height={20} />
-                        </PaddingButton>
-                    </>
-                    : null}
-                <Markdown title={props.text} fontSize={props.fontSize} />
-            </TouchableOpacity>
-        </Container >
-    );
-}
+  return (
+    <Container color={props.color} widthSize={props.widthSize}>
+      <TouchableOpacity
+        disabled={props.disabled}
+        onPress={props.onPress ? props.onPress : null}
+        style={{
+          alignItems: 'center',
+          justifyContent: 'center',
+          width: props.widthSize,
+          paddingTop: props.heightSize,
+          paddingBottom: props.heightSize,
+          flexDirection: 'row',
+        }}>
+        {props.text === 'Menu' ? (
+          <>
+            <PaddingButton>
+              <Image type="Menu" width={20} height={20} />
+            </PaddingButton>
+          </>
+        ) : null}
+        <Markdown title={props.text} fontSize={props.fontSize} />
+      </TouchableOpacity>
+    </Container>
+  );
+};
 
 export default React.memo(Button);
--- src/components/markdown/index.tsx
+++ src/components/markdown/index.tsx
@@ -1,30 +1,36 @@
-import React, { forwardRef } from 'react';
-import { Text, View, ShadowPropTypesIOS } from "react-native";
-import { MarkdownT } from './styles';
+import React, {forwardRef} from 'react';
+import {Text, View, ShadowPropTypesIOS} from 'react-native';
+import {MarkdownT} from './styles';
 
 interface IMarkdown {
-    title: string;
-    fontSize?: number;
-    fontColor?: string;
-    fontFamily?: string;
-    textAlign?: string;
-    lineHeight?: number;
+  title: string;
+  fontSize?: number;
+  fontColor?: string;
+  fontFamily?: string;
+  textAlign?: string;
+  lineHeight?: number;
 }
 
-const Markdown = ({ title, fontSize, fontColor, fontFamily, textAlign, lineHeight }: IMarkdown) => {
-    return (
-        <View>
-            <MarkdownT
-                fontSize={fontSize}
-                fontColor={fontColor}
-                fontFamily={fontFamily}
-                textAlign={textAlign}
-                lineHeight={lineHeight}
-            >
-                {title}
-            </MarkdownT>
-        </View>
-    );
-}
+const Markdown = ({
+  title,
+  fontSize,
+  fontColor,
+  fontFamily,
+  textAlign,
+  lineHeight,
+}: IMarkdown) => {
+  return (
+    <View>
+      <MarkdownT
+        fontSize={fontSize}
+        fontColor={fontColor}
+        fontFamily={fontFamily}
+        textAlign={textAlign}
+        lineHeight={lineHeight}>
+        {title}
+      </MarkdownT>
+    </View>
+  );
+};
 
 export default React.memo(Markdown);
